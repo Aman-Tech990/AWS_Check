28e2b41b3e6fa78cd2a0d9b683848d510069078a
Aman Parida <amantechz467@gmail.com>
2026-08-23 16:53:42 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import express, { json } from "express";
+import dotenv from "dotenv";
+
+if (process.env.NODE_ENV !== "production") {
+    dotenv.config({});
+}
+
+const app = express();
+const port = process.env.PORT || 3005;
+
+app.get("/health", (req, res) => {
+    return res.status(200).json({
+        success: true,
+        message: `API's working well!`
+    })
+});
+
+app.get("/", (req, res) => {
+    return res.status(200).json({
+        success: true,
+        message: `All good from ${port}!`
+    })
+});
+
+app.listen(port, () => {
+    console.log(`Server running at port ${port}`);
+});
